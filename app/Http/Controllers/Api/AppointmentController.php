fc70980dd17b044a66e611bfb0ef402f5f2513cf
DESKTOP-0AI4HU3\Edenilson <ruiz.edenilson@gmail.com>
2019-11-24 14:08:13 -0600

    Add use for Appointment Model

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\StoreAppointment;
 use App\Http\Controllers\Controller;
 use Auth;
+use App\Appointment;
 
 class AppointmentController extends Controller
 {
