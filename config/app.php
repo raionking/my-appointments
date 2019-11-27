e0e9245d91f748576725a755a8adced777118b57
DESKTOP-0AI4HU3\Edenilson <ruiz.edenilson@gmail.com>
2019-11-26 22:21:30 -0600

    Config fcm package kawankoding laravel-fcm

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         //Mi servicio de horarios
         App\Providers\ScheduleServiceProvider::class,
 
+        Kawankoding\Fcm\FcmServiceProvider::class,
+
     ],
 
     /*
@@ -210,6 +212,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Fcm' => Kawankoding\Fcm\FcmFacade::class,
 
     ],
 
