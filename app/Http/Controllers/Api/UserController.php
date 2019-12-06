19f5a9832ad14f10ae4cc5792f554cd1bc256754
DESKTOP-0AI4HU3\Edenilson <ruiz.edenilson@gmail.com>
2019-12-05 21:28:43 -0600

    Add function update user from api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -13,6 +13,15 @@ class UserController extends Controller
     	return Auth::guard('api')->user();
     }
 
+    public function update(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+        $user->save();
+    }
+
     public function logout()
     {
     	Auth::guard('api')->logout();
